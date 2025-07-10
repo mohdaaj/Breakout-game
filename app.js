072862f2fd360cec87212a50d2db8eaf66e29ffe
mohammed ali <hasoon7891@hotmail.com>
2025-07-10 16:48:55 +0300

    commit message goes here

--- app.js
+++ app.js
@@ -1,6 +1,27 @@
+const gameBoundary = document.getElementById("game-boundary");
 const paddle = document.getElementById("paddle");
+const ball = document.getElementById("ball");
 
 
-paddle.addEventListener('keydown', () => {
-  console.log('You clicked me!');
+gameBoundary.addEventListener("mousemove", function(event) {
+  //  taking the mouse position inside the boundary
+  const boundaryRect = gameBoundary.getBoundingClientRect();
+  const mouseX = event.clientX - boundaryRect.left;
+
+  //  centering the paddle for mouse
+  const paddleWidth = paddle.offsetWidth;
+  let newLeft = mouseX - paddleWidth / 2;
+
+  // making the paddle inside the boundary and not letting it getting out of it.
+  const maxLeft = gameBoundary.clientWidth - paddleWidth;
+  if (newLeft < 0) newLeft = 0;
+  if (newLeft > maxLeft) newLeft = maxLeft;
+
+  //  Applying the new position
+  paddle.style.left = newLeft + "px";
 });
+
+
+function moveBall() {
+    
+}
